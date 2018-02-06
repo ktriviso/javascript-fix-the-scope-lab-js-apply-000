5d66713d74362413751009231829e3c8b17391bb
Krista Triviso <krista.triviso91@gmail.com>
2018-02-06 18:52:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function add2(n) {
 add2();
 
 var funkyFunction = function() {
-  return function() {
+  return function theFunk () {
     return "FUNKY!"
   }
 }
@@ -29,3 +29,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction();
+var theFunk = theFunk();
